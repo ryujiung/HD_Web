67003afc12bd3934981360463d8bba6d87a7f129
Ryu Jiung <rju1202@naver.com>
2026-04-10 18:05:53 +0900

    error fix

---
+++ src/lib/brandInfo.ts
@@ -0,0 +1,46 @@
+export const brandInfo: Record<
+  string,
+  { image: string; description: string }
+> = {
+  우영미: {
+    image: "/image/wooyoungmi.jpg",
+    description:
+      "우영미는 미니멀하면서도 구조적인 실루엣이 특징인 한국 디자이너 브랜드입니다.",
+  },
+
+  르메르: {
+    image: "/image/lemaire.png",
+    description:
+      "르메르는 편안하면서도 세련된 미니멀 스타일로 유명한 프랑스 브랜드입니다.",
+  },
+
+  알로: {
+    image: "/image/alo.jpg",
+    description:
+      "알로는 애슬레저 기반의 편안하고 세련된 라이프스타일 브랜드입니다.",
+  },
+
+  베이프: {
+    image: "/image/bape.jpg",
+    description:
+      "베이프는 스트리트웨어 기반의 일본 브랜드로 강한 개성이 특징입니다.",
+  },
+
+  몽블랑: {
+    image: "/image/montblanc.jpg",
+    description:
+      "몽블랑은 고급 필기구와 가죽 제품으로 유명한 프리미엄 브랜드입니다.",
+  },
+
+  태그호이어: {
+    image: "/image/tagheuer.jpg",
+    description:
+      "태그호이어는 스포츠 감성의 스위스 럭셔리 시계 브랜드입니다.",
+  },
+
+  스와로브스키: {
+    image: "/image/swarovski.jpg",
+    description:
+      "스와로브스키는 크리스탈 액세서리로 유명한 브랜드입니다.",
+  },
+};
